e4a859458eb2cd5cb27867c03a3bbebb06ccc165
OptimisticLock <izgitlab@optimisticlock.com>
2016-03-01 18:14:49 -0800

    posts sorted

--- poster.js
+++ poster.js
@@ -5,7 +5,7 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     posts: function () {
-      return Posts.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
