ed3da93eda1e7984b70d5e7c134c4c87a13deb7b
OptimisticLock <izgitlab@optimisticlock.com>
2016-03-02 00:33:39 -0800

    posting rich text

--- client/poster.js
+++ client/poster.js
@@ -78,8 +78,41 @@ Template.mySummerNote.rendered = function () {
 };
 
 Template.mySummerNote.events({
-    "click #post": function () {
+    "ignore me": function () {
         var html = $('#summernote').summernote('code');
         console.log(html);
+    },
+
+    "click #post": function (event) {
+        // Prevent default browser form submit
+
+        console.log("New rich text post submitted");
+        event.preventDefault();
+
+        // Get value from form element
+        var html = $('#summernote').summernote('code');
+        console.log(html);
+
+        // If new post is not empty, insert it
+        if ($.trim(html) != '') {
+
+            var me = Meteor.user();
+
+            // me.sername if user logged in with password.
+            // me.profile.name if user logged in with Facebook and maybe other OAuth services.
+            var username = me.username || me.profile.name
+
+            // Insert a post into the collection
+            Posts.insert({
+                // TODO: this field should now be "html", not "text", but too lazy to do another db migration
+                text: html,
+                createdAt: new Date(),            // current time
+                author: Meteor.userId(),           // _id of logged in user
+                username: username  // username of logged in user
+            });
+
+            // Clear form
+         //   event.target.text.value = "";
+        }
     }
 });
