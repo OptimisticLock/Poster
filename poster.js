20a9260f78b50676f789d0eb7d35720ae9ea55bb
OptimisticLock <izgitlab@optimisticlock.com>
2016-03-01 18:09:21 -0800

    database access

--- poster.js
+++ poster.js
@@ -8,4 +8,23 @@ if (Meteor.isClient) {
       return Posts.find({});
     }
   });
+
+  Template.body.events({
+    "submit .new-post": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a post into the collection
+      Posts.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
 }
